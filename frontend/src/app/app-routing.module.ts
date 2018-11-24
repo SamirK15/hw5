180870da5e02e03a2579225f6a86116caf2af8f0
Ben <benheffernan16@hotmail.com>
2018-11-24 12:11:05 -0500

    added basic structure of todo page and skeleton todo service

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ToDoComponent } from './components/to-do/to-do.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'home-page', component: HomePageComponent },
   { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent }
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'to-do', component: ToDoComponent }
 ];
 
 @NgModule({
