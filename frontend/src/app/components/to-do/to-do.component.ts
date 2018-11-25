d69e788b585a207c83ef225d9783508722d97de2
sshaikh733 <salman-sheikh@live.ca>
2018-11-25 05:59:35 -0500

    polished some things up a bit on to-do and other components

--- frontend/src/app/components/to-do/to-do.component.ts
+++ frontend/src/app/components/to-do/to-do.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
 
 import { TaskService } from '../.././services/task/task.service';
+import {Time} from "@angular/common";
 
 @Component({
   selector: 'app-to-do',
@@ -11,11 +12,22 @@ export class ToDoComponent implements OnInit {
 
 
   heroes=[];
+  temp=[];
+  groups=[];
 
-  onClick(value:string){
-    if(value) {
-      this.heroes.push(value);
-    }
+  onClick(name:string,date:Date,time:Time){
+      /*this.temp.push(name,date,time);
+      this.heroes.push(this.temp.toString())*/
+      if(name&&date&&time) {
+        this.heroes.push("Task: " + name + " Due: " + date + " at " + time);
+      }
+      else {
+        window.alert("Invalid Entry")
+      }
+  }
+
+  gCreate(){
+    this.groups.push("New Group");
   }
 
   ngOnInit() {
